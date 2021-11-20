f2b73060afeab3b337a7c466f7e6aca0ecb9e655
alexn34-n <alexn3469@mail.ru>
2021-11-20 16:20:08 +0300

    Initial commit

---
+++ src/main/java/com/ship/shipshop5/Shipshop5Application.java
@@ -0,0 +1,13 @@
+package com.ship.shipshop5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shipshop5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shipshop5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ship/shipshop5/Shipshop5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ship.shipshop5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shipshop5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
